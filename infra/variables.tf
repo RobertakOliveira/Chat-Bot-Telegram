8e519042180bb3a941325c6d3b7857de1a45fbd8
Katcilane silva de souza <143912823+KatSilvax@users.noreply.github.com>
2025-04-07 15:56:39 -0400

    refactor: reajuste de código

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,14 @@
 # Declaração de variáveis para o módulo de infraestrutura (sem variáveis de credenciais)
+
+variable "aws_region" {
+    description = "Região da AWS onde os recursos serão criados"
+    type       = string  # Tipo da variável: string
+    default   = "us-east-1"  # Região padrão da AWS
+}
+
+variable "owner_tag" {
+    description = "Indentificador do dono (sem dados pessoais)"
+    type     = string  # Tipo da variável: string
+    default = "katcilane"  # Identificador padrão do dono
+    
+}
