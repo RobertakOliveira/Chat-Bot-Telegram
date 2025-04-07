17f55221022dbaac9f748d23da9eaafbbe475e8b
Katcilane silva de souza <143912823+KatSilvax@users.noreply.github.com>
2025-04-07 13:59:19 -0400

    feat(provider): Configura provider AWS genérico (sem credenciais) Define uma configuração genérica para o provider AWS no nível raiz, utilizando apenas a variável de região, isso centraliza a configuração do provider e evita a repetição da mesma em cada módulo, facilitando a manutenção e garantindo que as credenciais não sejam incluídas diretamente na configuração. A responsabilidade de fornecer as credenciais (via variáveis de ambiente, arquivos de configuração ou roles IAM) é delegada ao ambiente de execução.

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,1 +1,6 @@
 # Configuração genérica do provider (sem credenciais) assim não será necessário repetir o provider em cada módulo
+
+provider "aws" {
+    region = var.aws_region
+    # A configuração do provider AWS não deve conter credenciais sensíveis, como access_key e secret_key.
+}
