7e0608ea51c902e398951592e4077e86987e32c3
Katcilane silva de souza <143912823+KatSilvax@users.noreply.github.com>
2025-04-07 14:08:35 -0400

    fix: alteração de observação

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,6 +1,7 @@
 # Configuração genérica do provider (sem credenciais) assim não será necessário repetir o provider em cada módulo
 
-provider "aws" {
+provider "aws" {      # O provider AWS é utilizado para interagir com os serviços da AWS.
+    
     region = var.aws_region
     # A configuração do provider AWS não deve conter credenciais sensíveis, como access_key e secret_key.
 }
