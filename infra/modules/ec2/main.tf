f6c21808f0953535c67f911b9e8db44b728e8d69
Katcilane silva de souza <143912823+KatSilvax@users.noreply.github.com>
2025-04-07 15:55:19 -0400

    refactor: reajuste de código

--- infra/modules/ec2/main.tf
+++ infra/modules/ec2/main.tf
@@ -1,2 +1,13 @@
 ## Recursos com tags + random suffixes, significa que os recursos criados terão sufixos aleatórios adicionados aos seus nomes.
 # # Isso é importante para garantir que os recursos sejam únicos e evitar conflitos de nomes.
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 4  # Sufixo randômico de 4 bytes 
+}
+
+resource "aws_s3_bucket" "docs" {
+  bucket = "chatbot-docs-${random_id.bucket_suffix.hex}"  # Nome imprevisível do bucket S3 
+  tags   = {
+    Owner = var.owner_tag  # Rastreamento interno apenas 
+  }
+}
