02b044ab60c6522e51368b2d0518fbd9a5ac26ec
Katcilane silva de souza <143912823+KatSilvax@users.noreply.github.com>
2025-04-07 15:54:38 -0400

    refactor: reajustes de código

--- infra/modules/ec2/iam.tf
+++ infra/modules/ec2/iam.tf
@@ -1,2 +1,16 @@
 # Policies com least privilege, significa que as permissões concedidas são as mínimas necessárias para realizar as operações necessárias.
 # Isso é importante para garantir a segurança e a conformidade, minimizando o risco de acesso não autorizado ou ações indesejadas.
+
+resource "aws_iam_policy" "bedrock_access" {
+  name        = "bedrock-${random_id.policy_suffix.hex}"  # Nome não descritivo
+  description = "Permissões mínimas para Bedrock"
+
+  policy = jsonencode({       # Aplicar a politica de IAM
+    Version = "2012-10-17",
+    Statement = [{
+      Action   = ["bedrock:InvokeModel"],  # Escopo restrito
+      Effect   = "Allow",
+      Resource = "*"
+    }]
+  })
+}
