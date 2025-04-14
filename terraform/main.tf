86eea04b234fc3dea2f9bb654c57b38b19f6fbab
guirque <guirque@gmail.com>
2025-04-14 14:10:41 -0300

    build: adicionando EC2 em arquivo main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,19 @@ module "s3" {
   source = "./s3"
   bucket_name = var.bucket_name
 }
+
+variable "ssh_ip" {
+    description = "IP to allow for SSH connections to the EC2 instance."
+    type = string
+}
+
+variable "key_name" {
+    description = "Key to connect to instance via SSH."
+    type = string
+}
+
+module "ec2" {
+    source = "./ec2"
+    ssh_ip = var.ssh_ip
+    key_name = var.key_name
+}
