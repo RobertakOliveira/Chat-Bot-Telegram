d16386db35ee38d487050ae9e6615e549caef6e9
guirque <guirque@gmail.com>
2025-04-14 14:15:29 -0300

    build: adicionando instalação de Docker a start-script.sh

---
+++ terraform/ec2/start-script.sh
@@ -0,0 +1,4 @@
+# Installing and Running Docker (taken from https://docs.aws.amazon.com/pt_br/serverless-application-model/latest/developerguide/install-docker.html) ----------
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
