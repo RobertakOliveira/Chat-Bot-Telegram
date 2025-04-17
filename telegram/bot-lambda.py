d5417cb9140282d02b3c702af40f389ca00ade31
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-17 16:12:50 -0300

    ♻️ refactor: Removendo código desnecessário e ajustando a função de resposta do bot

--- telegram/bot-lambda.py
+++ telegram/bot-lambda.py
@@ -1,24 +1,5 @@
 import json
-import urllib3
-
-#token do bot
-BOT_TOKEN="SEU-TOKEN-AQUI"
-
-#resposta para cada request enviado pelo usuario do bot
-def sendReply(chat_id, message):
-    reply = {
-        "chat_id": chat_id,
-        "text": message#retorna a mensagem enviada pelo usuario
-    }
-
-    http = urllib3.PoolManager()
-    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
-    encoded_data = json.dumps(reply).encode('utf-8')
-    http.request('POST', url, body=encoded_data, headers={'Content-Type': 'application/json'})
-    #print para monitorar o retorno do bot
-    print(f"*** Reply : {encoded_data}")
-
-#função principal que recebe o evento do telegram extraindo a mensagem enviada pelo usuario.
+from send_Reply  import sendReply
 def lambda_handler(event, context):
     body = json.loads(event['body'])
 
@@ -27,15 +8,18 @@ def lambda_handler(event, context):
     chat_id = body['message']['chat']['id']
     user_name = body['message']['from']['username']
     message_text = body['message']['text']
+    message_id = body['message']['message_id'] 
 
     print(f"*** chat id: {chat_id}")
     print(f"*** user name: {user_name}")
     print(f"*** message text: {message_text}")
     print(json.dumps(body))
 
-    reply_message = f"Reply to {message_text}"
+    # aqui chama o rag 
+    reply_message = message_text
+    
 
-    sendReply(chat_id, reply_message)
+    sendReply(chat_id, reply_message,message_id)
 
     return {
         'statusCode': 200,
