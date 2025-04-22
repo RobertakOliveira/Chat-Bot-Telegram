dedd88a623d94417108b68a2940f6dda138f8769
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 20:58:47 -0400

    ✨ feat: Adiciona variáveis para configuração da função Lambda 1

---
+++ pipelines/build_step_functions/modules/apigateway/variables.tf
@@ -0,0 +1,9 @@
+variable "lambda1_invoke_arn" {
+  description = "ARN de invocação da Lambda 1"
+  type        = string
+}
+
+variable "lambda1_name" {
+  description = "Nome da função Lambda 1 para liberar permissão via API Gateway"
+  type        = string
+}
