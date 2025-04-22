537f2158540f88a1cf2ecb3818caf12da26b8506
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 21:03:03 -0400

    ♻️ refactor: Remove saídas do ECR repository URL da main.tf

--- pipelines/build_step_functions/modules/ecr/main.tf
+++ pipelines/build_step_functions/modules/ecr/main.tf
@@ -2,7 +2,3 @@ resource "aws_ecr_repository" "lambda_ecr" {
   name          = "lambda-docker-repo"
   force_delete  = true
 }
-
-output "repository_url" {
-  value = aws_ecr_repository.lambda_ecr.repository_url
-}
