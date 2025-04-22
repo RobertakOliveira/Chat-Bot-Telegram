688b737bfde059c18f4ea65c39e6d70b24db0d79
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 22:42:40 -0400

    🐛 fix: Corrige nomes das variáveis lambda_invoke_arn e lambda_invoke_name no módulo apigateway

--- pipelines/build_step_functions/modules/apigateway/variables.tf
+++ pipelines/build_step_functions/modules/apigateway/variables.tf
@@ -1,9 +1,9 @@
-variable "lambda1_invoke_arn" {
+variable "lambda_invoke_arn" {
   description = "ARN de invocação da Lambda 1"
   type        = string
 }
 
-variable "lambda1_name" {
+variable "lambda_invoke_name" {
   description = "Nome da função Lambda 1 para liberar permissão via API Gateway"
   type        = string
 }
