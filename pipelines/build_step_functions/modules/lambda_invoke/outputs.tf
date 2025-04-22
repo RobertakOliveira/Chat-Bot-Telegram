def3cb19280aa7b114987963f3b91669a1e2ca53
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 22:44:57 -0400

    ✨ feat: Adiciona saídas para invoke_arn, function_name e arn no módulo lambda_invoke

---
+++ pipelines/build_step_functions/modules/lambda_invoke/outputs.tf
@@ -0,0 +1,12 @@
+output "invoke_arn" {
+  value = aws_lambda_function.lambda_invoke.invoke_arn
+}
+
+output "function_name" {
+  value = aws_lambda_function.lambda_invoke.function_name
+}
+
+output "arn" {
+  value = aws_lambda_function.lambda_invoke.arn
+}
+
