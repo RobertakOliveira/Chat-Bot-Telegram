10d6fd9643995667eb6f3b957a60dfaa5fbf2540
guirque <guirque@gmail.com>
2025-04-22 13:20:29 -0300

    build: configurando EC2 em subrede pública

--- terraform/ec2/ec2.tf
+++ terraform/ec2/ec2.tf
@@ -1,15 +1,3 @@
-# VARS ---------------------------------------------------------------------
-
-variable "key_name" {
-    description = "Key to connect to instance via SSH."
-    type = string
-}
-
-variable "ssh_ip" {
-    description = "IP to allow for SSH connections to the EC2 instance."
-    type = string
-}
-
 # INSTANCE -----------------------------------------------------------------
 
 resource "aws_instance" "ec2" {
@@ -23,43 +11,72 @@ resource "aws_instance" "ec2" {
     "Name" = "Chatbot EC2"
   }
 
-  key_name = var.key_name
-
   volume_tags = {
     "Project" = ""
     "CostCenter" = ""
     "Name" = "Chatbot EC2"
   }
 
-  user_data_base64 = filebase64("./ec2/start-script.sh")
+  user_data = file("./ec2/start-script.sh")
   security_groups = [aws_security_group.ec2_sg.id]
-  subnet_id = aws_subnet.chatbot_subnet.id
+  subnet_id = aws_subnet.chatbot_public_subnet.id
 }
 
 # VPC / SUBNET --------------------------------------------------------------
 
 # VPC
 resource "aws_vpc" "chatbot_vpc" {
-    cidr_block = "10.0.0.0/16"
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        "Name" = "Chatbot VPC"
+    }
 }
 
 # Internet Gateway
 resource "aws_internet_gateway" "igw" {
     vpc_id = aws_vpc.chatbot_vpc.id
+
+    tags = {
+      "Name" = "Chatbot Internet Gateway"
+    }
 }
 
-# Route
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+    vpc_id = aws_vpc.chatbot_vpc.id
+
+    tags = {
+        "Name" = "Chatbot Public Route Table"
+    }
+}
+
+# Public Route Table Route to Internet Gateway
 resource "aws_route" "redirect_to_igw_route" {
-    route_table_id = aws_vpc.chatbot_vpc.main_route_table_id # VPC's default route table
+    route_table_id = aws_route_table.public_route_table.id # Public Route Table
     gateway_id = aws_internet_gateway.igw.id # Redirect incoming traffic to internet gateway
     destination_cidr_block = "0.0.0.0/0" # All traffic
 }
 
-# Public subnet (route table is created automatically on the VPC). EC2 is associated with the subnet.
-resource "aws_subnet" "chatbot_subnet" {
+# Chatbot (Public) Subnet
+resource "aws_subnet" "chatbot_public_subnet" {
     vpc_id = aws_vpc.chatbot_vpc.id
-    cidr_block = "10.0.0.0/24"
+    cidr_block = "10.0.0.0/25"
     map_public_ip_on_launch = true
+
+    tags = {
+      "Name" = "APIGW Public Subnet"
+    }
+}
+resource "aws_route_table_association" "public_subnet_association" { # necessary to bind to correct route table
+  route_table_id = aws_route_table.public_route_table.id
+  subnet_id = aws_subnet.chatbot_public_subnet.id
+}
+
+# EC2 Connect Endpoint -----------------------------------------------------
+resource "aws_ec2_instance_connect_endpoint" "chatbot_ec2_connect" {
+  subnet_id = aws_subnet.chatbot_public_subnet.id
+  security_group_ids = [aws_security_group.ec2_sg.id]
 }
 
 # SECURITY GROUP -----------------------------------------------------------
@@ -73,7 +90,7 @@ resource "aws_security_group" "ec2_sg" {
 resource "aws_vpc_security_group_ingress_rule" "allow_http_ir" {
     security_group_id = aws_security_group.ec2_sg.id
     ip_protocol = "tcp"
-    cidr_ipv4 = "0.0.0.0/0"
+    cidr_ipv4 = "0.0.0.0/0" # change this to Telegram's IP
     from_port = 80
     to_port = 80
 }
@@ -81,9 +98,9 @@ resource "aws_vpc_security_group_ingress_rule" "allow_http_ir" {
 resource "aws_vpc_security_group_ingress_rule" "allow_ssh_ir" {
     security_group_id = aws_security_group.ec2_sg.id
     ip_protocol = "tcp"
-    cidr_ipv4 = var.ssh_ip
     from_port = 22
     to_port = 22
+    referenced_security_group_id = aws_security_group.ec2_sg.id # needed to allow EC2 endpoint connection
 }
 
 resource "aws_vpc_security_group_egress_rule" "sg_er_1" {
