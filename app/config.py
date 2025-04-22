31fac921640504ac0b5a273621982a31731e2e40
Leon Dale <leon_rabello@id.uff.br>
2025-04-22 17:53:11 -0300

    ♻️ refactor: movendo .env para diretorio raiz.

--- app/config.py
+++ app/config.py
@@ -1,9 +1,10 @@
 import os
 from dotenv import load_dotenv
-
-# Carregar o arquivo .env
-load_dotenv()
-
+from pathlib import Path
+ 
+# Carregue o arquivo .env da raiz do projeto
+env_path = Path(__file__).resolve().parent.parent / ".env"  # Caminho relativo para a raiz
+load_dotenv(dotenv_path=env_path)  # Passa o caminho para o load_dotenv()
 # Definir as variáveis necessárias
 API_URL = os.getenv("API_URL")
 API_SECRET_KEY = os.getenv("API_SECRET_KEY")
