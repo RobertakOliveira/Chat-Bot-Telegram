9328ba93232312b2f2053cccc012d39b98885c8a
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 21:05:34 -0400

    ♻️ refactor: Remove variáveis de saída do IAM roles em main.tf

--- pipelines/build_step_functions/modules/iam/main.tf
+++ pipelines/build_step_functions/modules/iam/main.tf
@@ -30,11 +30,3 @@ resource "aws_iam_role" "step_function_role" {
     }]
   })
 }
-
-output "lambda_exec_role_arn" {
-  value = aws_iam_role.lambda_exec_role.arn
-}
-
-output "step_function_role_arn" {
-  value = aws_iam_role.step_function_role.arn
-}
