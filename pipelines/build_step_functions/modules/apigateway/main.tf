0ee28f21aa25f1a1c496cc493d95bfe98123bfb9
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 20:31:44 -0400

    ♻️ refactor:  Módulo para integração do API Gateway com funções Lambda

---
+++ pipelines/build_step_functions/modules/apigateway/main.tf
@@ -0,0 +1,48 @@
+variable "lambda1_invoke_arn" {
+  type = string
+}
+
+variable "lambda1_name" {
+  type = string
+}
+
+resource "aws_apigatewayv2_api" "lambda_api" {
+  name          = "lambda-entry-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id                 = aws_apigatewayv2_api.lambda_api.id
+  integration_type       = "AWS_PROXY"
+  integration_uri        = var.lambda1_invoke_arn
+  integration_method     = "POST"
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "lambda_route" {
+  api_id    = aws_apigatewayv2_api.lambda_api.id
+  route_key = "POST /lambda-entry"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.lambda_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+resource "aws_lambda_permission" "apigw_invoke" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda1_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/POST/lambda-entry"
+}
+
+output "api_url" {
+  value = "${aws_apigatewayv2_api.lambda_api.api_endpoint}/lambda-entry"
+}
+
+output "execution_arn" {
+  value = aws_apigatewayv2_api.lambda_api.execution_arn
+}
