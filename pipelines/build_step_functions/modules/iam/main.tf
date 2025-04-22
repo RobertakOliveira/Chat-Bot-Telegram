346d80ecaab28652c8392441b88bce4104166d64
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 20:15:24 -0400

    ✨ feat: Módulo de roles IAM para funções Lambda e Step Functions

---
+++ pipelines/build_step_functions/modules/iam/main.tf
@@ -0,0 +1,40 @@
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      },
+      Effect = "Allow"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role" "step_function_role" {
+  name = "step-function-role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect = "Allow",
+      Principal = {
+        Service = "states.amazonaws.com"
+      },
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+output "lambda_exec_role_arn" {
+  value = aws_iam_role.lambda_exec_role.arn
+}
+
+output "step_function_role_arn" {
+  value = aws_iam_role.step_function_role.arn
+}
