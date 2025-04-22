aa65a95cef9ea0faadbaf008b833afc6947ad8a2
moises-creator <moises.ximenes5@gmail.com>
2025-04-21 22:42:27 -0400

    🐛 fix: Corrige referências de variáveis lambda_invoke_arn e lambda_invoke_name no módulo apigateway

--- pipelines/build_step_functions/modules/apigateway/main.tf
+++ pipelines/build_step_functions/modules/apigateway/main.tf
@@ -6,7 +6,7 @@ resource "aws_apigatewayv2_api" "lambda_api" {
 resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id                 = aws_apigatewayv2_api.lambda_api.id
   integration_type       = "AWS_PROXY"
-  integration_uri        = var.lambda1_invoke_arn
+  integration_uri        = var.lambda_invoke_arn
   integration_method     = "POST"
   payload_format_version = "2.0"
 }
@@ -26,7 +26,7 @@ resource "aws_apigatewayv2_stage" "default_stage" {
 resource "aws_lambda_permission" "apigw_invoke" {
   statement_id  = "AllowAPIGatewayInvoke"
   action        = "lambda:InvokeFunction"
-  function_name = var.lambda1_name
+  function_name = var.lambda_invoke_name
   principal     = "apigateway.amazonaws.com"
   source_arn    = "${aws_apigatewayv2_api.lambda_api.execution_arn}/*/POST/lambda-entry"
 }
