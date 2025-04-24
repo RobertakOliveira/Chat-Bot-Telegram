9cb8fa5a8929ec18a11ffce3679b3e1e3462cf08
Caio Dias Ferreira <cai0ferreira@hotmail.com>
2025-04-24 17:00:39 -0300

    🔧chore: adicionando importação necessária

--- pipelines/lambda_RAG/lambda_rag.py
+++ pipelines/lambda_RAG/lambda_rag.py
@@ -2,6 +2,7 @@
 
 import os
 import boto3
+import shutil
 from langchain_community.document_loaders import DirectoryLoader, PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
