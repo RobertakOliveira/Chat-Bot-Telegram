72e03d1944417b1ddc72215b25ca83da89ee4b69
moises-creator <moises.ximenes5@gmail.com>
2025-04-24 22:28:22 -0400

    ✨ feat: Adiciona saídas para URL da API e ARN de execução no Terraform

---
+++ pipelines/build_RAG/api/outputs.tf
@@ -0,0 +1,9 @@
+output "api_url" {
+  description = "Endpoint público da API Gateway"
+  value = "${aws_apigatewayv2_api.lambda_api.api_endpoint}/webhook"
+}
+
+output "execution_arn" {
+  description = "ARN usado para configurar permissões de invocação na Lambda"
+  value       = aws_apigatewayv2_api.lambda_api.execution_arn
+}
