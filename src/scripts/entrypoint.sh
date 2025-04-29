f7c9a4aff3d80078729e99c3880b1d7cef8bded5
Diogo Valongo <64019570+diogo-valongo@users.noreply.github.com>
2025-04-29 17:01:27 -0300

    feat: garantindo execução procedural dos scripts

--- src/scripts/entrypoint.sh
+++ src/scripts/entrypoint.sh
@@ -1,5 +1,6 @@
-#! bin/bash
+#!/bin/bash
 
-python3 src/scripts/init_chroma.py
-
+# Executar o script de inicialização e aguardar sua conclusão
+python3 /src/scripts/init_chroma.py && \
+# Somente após a conclusão bem-sucedida, iniciar o gunicorn
 gunicorn --workers=2 --chdir=/src/ main:app -b unix:/shared/chatbotsocket.sock
