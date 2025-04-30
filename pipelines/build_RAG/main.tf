6d52ecef215fd52080028577e72b5aef5b2eae15
moises-creator <moises.ximenes5@gmail.com>
2025-04-30 18:43:35 -0400

    ♻️refactor: Reorganizando a estrutura dos módulos no arquivo main.tf

--- pipelines/build_RAG/main.tf
+++ pipelines/build_RAG/main.tf
@@ -1,22 +1,18 @@
 provider "aws" {
-    region = "us-east-1"
+  region = "us-east-1"
 }
 
 module "ecr" {
-    source = "./ecr"
+  source = "./ecr"
 }
 
 module "iam" {
   source = "./iam"
 }
 
-module "api" {
-  source = "./api"
-
-  lambda_telegram_arn  = module.lambda.lambda_telegram_arn
-  lambda_telegram_name = module.lambda.lambda_telegram_name
+module "s3" {
+  source = "./s3"
 }
-
 
 module "lambda" {
   source = "./lambda"
@@ -30,6 +26,13 @@ module "lambda" {
   ecr_repo_url   = module.ecr.repository_url
   exec_role_arn  = module.iam.lambda_exec_role_arn
   aws_region     = var.aws_region
+  bucket_name    = module.s3.bucket_name
 }
 
 
+module "api" {
+  source = "./api"
+
+  lambda_telegram_arn  = module.lambda.lambda_telegram_arn
+  lambda_telegram_name = module.lambda.lambda_telegram_name
+}
