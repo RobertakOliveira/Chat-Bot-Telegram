fc655df595d61a823546c75e41117050e12bb3db
Ricardo Menzes Bandeira <ricardobandeira197@gmail.com>
2025-04-30 19:27:10 -0300

    🚀feat: Atualizando a função sendReply para enviar a mensagem completa

--- pipelines/build_RAG/application/send_Reply.py
+++ pipelines/build_RAG/application/send_Reply.py
@@ -13,7 +13,7 @@ def sendReply(chat_id, full_message, _original_message_id):
     url_send = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
     initial_data = {
         "chat_id": chat_id,
-        "text": "..."  # pode deixar três pontinhos pra parecer digitando
+        "text": full_message # pode deixar três pontinhos pra parecer digitando
     }
     encoded_data = json.dumps(initial_data).encode('utf-8')
     response = http.request('POST', url_send, body=encoded_data, headers={'Content-Type': 'application/json'})
@@ -27,25 +27,25 @@ def sendReply(chat_id, full_message, _original_message_id):
         print(resp_json)
         return
 
-    message_id = resp_json["result"]["message_id"]
+    #message_id = resp_json["result"]["message_id"]
 
     # Agora edita a mensagem palavra por palavra
-    words = full_message.split()
-    current_text = ""
+    #words = full_message.split()
+    #current_text = ""
 
-    for word in words:
-        current_text += word + " "
+    #for word in words:
+    #    current_text += word + " "
 
-        edit_data = {
-            "chat_id": chat_id,
-            "message_id": message_id,
-            "text": current_text.strip()
-        }
+        #edit_data = {
+        #    "chat_id": chat_id,
+        #    "message_id": message_id,
+        #    "text": current_text.strip()
+        #}
 
-        encoded_edit = json.dumps(edit_data).encode('utf-8')
-        url_edit = f"https://api.telegram.org/bot{BOT_TOKEN}/editMessageText"
-        http.request('POST', url_edit, body=encoded_edit, headers={'Content-Type': 'application/json'})
+        #encoded_edit = json.dumps(edit_data).encode('utf-8')
+        #url_edit = f"https://api.telegram.org/bot{BOT_TOKEN}/editMessageText"
+        #http.request('POST', url_edit, body=encoded_edit, headers={'Content-Type': 'application/json'})
 
-        time.sleep(0.1)  # Delay entre palavras
+        #time.sleep(0.1)  # Delay entre palavras
 
-    print(f"*** Streamed message: {full_message}")
+    #print(f"*** Streamed message: {full_message}")
