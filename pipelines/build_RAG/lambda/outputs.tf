dfd842afeb2faa2cce9bde8031630db4b9b0ee05
moises-creator <moises.ximenes5@gmail.com>
2025-05-03 00:47:25 -0400

    🗑️remove: exclui arquivo lambda/outputs.tf da pipeline build_RAG

--- pipelines/build_RAG/lambda/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "lambda_telegram_arn" {
-  value = aws_lambda_function.telegram_rag.invoke_arn
-}
-
-output "lambda_telegram_name" {
-  value = aws_lambda_function.telegram_rag.function_name
-}
