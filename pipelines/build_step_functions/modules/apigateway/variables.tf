8f9567c12c13c0b403a3ae6e02365f2d1eba70a7
moises-creator <moises.ximenes5@gmail.com>
2025-05-03 00:49:46 -0400

    🗑️remove: exclui arquivo apigateway/variables.tf da pipeline build_step_functions

--- pipelines/build_step_functions/modules/apigateway/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "lambda_invoke_arn" {
-  description = "ARN de invocação da Lambda 1"
-  type        = string
-}
-
-variable "lambda_invoke_name" {
-  description = "Nome da função Lambda 1 para liberar permissão via API Gateway"
-  type        = string
-}
