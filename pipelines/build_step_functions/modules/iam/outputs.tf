f55e58411384428a4d70d921b1df9e1b0470af7d
moises-creator <moises.ximenes5@gmail.com>
2025-05-03 00:50:18 -0400

    🗑️remove: exclui arquivo outputs.tf do módulo iam da pipeline build_step_functions

--- pipelines/build_step_functions/modules/iam/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "lambda_exec_role_arn" {
-  description = "ARN da IAM Role de execução da função Lambda"
-  value       = aws_iam_role.lambda_exec_role.arn
-}
-
-output "step_function_role_arn" {
-  description = "ARN da IAM Role de execução da Step Function"
-  value       = aws_iam_role.step_function_role.arn
-}
