6a691f803848197cfbe476927a45f957869974fc
moises-creator <moises.ximenes5@gmail.com>
2025-05-03 00:45:09 -0400

    🗑️remove: exclui arquivo api/variables.tf da pipeline build_RAG

--- pipelines/build_RAG/api/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "lambda_telegram_arn" {
-  description = "ARN de invocação da Lambda Telegram"
-  type        = string
-}
-
-variable "lambda_telegram_name" {
-  description = "Nome da função Lambda Telegram para liberar permissão via API Gateway"
-  type        = string
-}
