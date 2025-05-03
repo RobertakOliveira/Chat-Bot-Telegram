ab586e03eafaf31dccf9e86e3b99eb5ffc46b367
moises-creator <moises.ximenes5@gmail.com>
2025-05-03 00:51:19 -0400

    🚀feat: adiciona variáveis embedding_db_arn e embedding_db_name no arquivo variables.tf do módulo s3

---
+++ pipelines/s3/variables.tf
@@ -0,0 +1,9 @@
+variable "embedding_db_arn" {
+  description = "ARN da função Lambda que gera embeddings"
+  type        = string
+}
+
+variable "embedding_db_name" {
+  description = "Nome da função Lambda que gera embeddings"
+  type        = string
+}
